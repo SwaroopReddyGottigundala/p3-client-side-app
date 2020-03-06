1658c70127a187ae865c87b956d0add819213b9c
swaroop reddy <reddy.swaroop2016@gmail.com>
2020-03-06 17:47:41 -0600

    initial

---
+++ jscript.js
@@ -0,0 +1,19 @@
+function restoGrandtotal()
+{
+     const first=document.querySelector("#name").value
+     const ans=getlength(first)
+         
+     
+     
+     document.getElementById("namecount").innerHTML = ans  
+
+
+}
+
+const getlength=(mystr)=>{return mystr.length}
+
+
+
+
+
+
